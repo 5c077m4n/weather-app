d7dcc8f00bb6ef7c9c81c7b3fc464f10ce8bff91
lighttall <ta8729@gmail.com>
2022-06-30 16:57:24 +0300

    initial commit

---
+++ front/src/components/Home/Home.js
@@ -0,0 +1,105 @@
+import React, { useState } from "react";
+import {
+  Grid,
+  Grow,
+  Container,
+  AppBar,
+  TextField,
+  Button,
+  Paper,
+} from "@material-ui/core";
+import { useHistory, useLocation } from "react-router-dom";
+import { useDispatch } from "react-redux";
+import { getPostsBySearch } from "../../actions/posts";
+
+import Posts from "../Posts/Posts";
+import Form from "../Form/Form";
+import Paginate from "../Paginate";
+import useStyle from "./style";
+import ChipInput from "material-ui-chip-input";
+
+function useQuery() {
+  return new URLSearchParams(useLocation().search);
+}
+
+const Home = () => {
+  const [currentId, setCurrentId] = useState(0);
+  const dispatch = useDispatch();
+  const query = useQuery();
+  const history = useHistory();
+  const page = query.get("page") || 1;
+  const searchQuery = query.get("searchQuery");
+  const classes = useStyle();
+  const [search, setSearch] = useState("");
+  const [tags, setTags] = useState([]);
+
+  const searchPost = () => {
+    if (search.trim() || tags) {
+      dispatch(getPostsBySearch({ search, tags: tags.join(",") }));
+      history.push(
+        `/posts/search?searchQuery=${search || "none"}&tags=${tags.join(",")}`
+      );
+    } else {
+      history.push("/");
+    }
+  };
+  const handleAdd = (tag) => setTags([...tags, tag]);
+
+  const handleDelete = (tagToDelete) =>
+    setTags(tags.filter((tag) => tag !== tagToDelete));
+
+  return (
+    <Grow in>
+      <Container maxWidth="xl">
+        <Grid
+          container
+          justifyContent="space-between"
+          alignItems="stretch"
+          className={classes.gridContainer}
+          spacing={3}
+        >
+          <Grid item xs={12} sm={6} md={9}>
+            <Posts setCurrentId={setCurrentId} />
+          </Grid>
+          <Grid item xs={12} sm={6} md={3}>
+            <AppBar
+              className={classes.appBarSearch}
+              position="static"
+              color="inherit"
+            >
+              <TextField
+                name="search"
+                label="weather Search"
+                variant="outlined"
+                fullWidth
+                value={search}
+                onChange={(e) => {
+                  setSearch(e.target.value);
+                }}
+              />
+              <ChipInput
+                value={tags}
+                style={{ margin: "10px, 0" }}
+                onAdd={handleAdd}
+                onDelete={handleDelete}
+                variant="outlined"
+                label="search tags"
+              />
+              <Button variant="contained" color="primary" onClick={searchPost}>
+                Search
+              </Button>
+            </AppBar>
+            <Form currentId={currentId} setCurrentId={setCurrentId} />
+            {!searchQuery && !tags.length && (
+              <Paper elevation={6} className={classes.paginate}>
+                <Paginate page={page} />
+              </Paper>
+            )}
+          </Grid>
+        </Grid>
+      </Container>
+    </Grow>
+  );
+};
+
+export default Home;
---
+++ front/src/components/auth/Input.jsx
@@ -0,0 +1,48 @@
+import React from "react";
+import { Grid, TextField, InputAdornment, IconButton } from "@material-ui/core";
+import Visibility from "@material-ui/icons/Visibility";
+import VisibilityOff from "@material-ui/icons/VisibilityOff";
+
+const Input = ({
+  half,
+  handleChange,
+  handleShowPassword,
+  name,
+  label,
+  autoFocus,
+  type,
+}) => {
+  return (
+    <Grid item xs={12} sm={half ? 6 : 12}>
+      <TextField
+        name={name}
+        label={label}
+        onChange={handleChange}
+        fullWidth
+        required
+        variant="outlined"
+        autoFocus={autoFocus}
+        type={type}
+        InputProps={
+          name === "password"
+            ? {
+                endAdornment: (
+                  <InputAdornment position="end">
+                    <IconButton onClick={handleShowPassword}>
+                      {type === "password " ? (
+                        <Visibility />
+                      ) : (
+                        <VisibilityOff />
+                      )}
+                    </IconButton>
+                  </InputAdornment>
+                ),
+              }
+            : null
+        }
+      />
+    </Grid>
+  );
+};
+
+export default Input;
